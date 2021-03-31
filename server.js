dfe30be7c539e8409434cbc69d1265f740d53b06
Serge <isserge@protonmail.com>
2021-03-31 17:41:16 +0800

    separated app from server

--- server.js
+++ server.js
@@ -1,24 +1,16 @@
 'use strict';
 
-require('dotenv').config();
-const fastify = require('fastify');
+const app = require('./app');
 const config = require('./config');
-const blockRoutes = require('./lib/block/routes');
-const accountRoutes = require('./lib/account/routes');
-const transactionRoutes = require('./lib/transaction/routes');
 
-const app = fastify({
+const server = app({
   logger: { prettyPrint: true },
 });
 
-app.register(blockRoutes, { prefix: '/api/block' });
-app.register(accountRoutes, { prefix: '/api/account' });
-app.register(transactionRoutes, { prefix: '/api/transaction' });
-
-app.listen(config.port, (err, address) => {
+server.listen(config.port, (err, address) => {
   if (err) {
-    app.log.error(err);
+    server.log.error(err);
     process.exit(1);
   }
-  app.log.info(`server listening on ${address}`);
+  server.log.info(`server listening on ${address}`);
 });
